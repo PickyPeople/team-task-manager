8389fc20ed1e7869abfca3b5ca43063d41acbce6
장재영 <jangjaeyeong@jangjaeyeong-ui-MacBookPro.local>
2025-05-08 00:42:22 +0900

    feat: add login error message

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,56 +5,59 @@
       h2.mt-2.mb-4.--bs-tertiary-bg Workflow
   
       form(@submit.prevent="handleLogin")
-        .mb-3.text-start
+        div.mb-3.text-start
           label.form-label(for="email") 이메일
           input#email.form-control(type="email" v-model="email" required placeholder="example@email.com")
         
-        .mb-3.text-start
+        div.mb-3.text-start
           label.form-label(for="password") 비밀번호
           input#password.form-control(type="password" v-model="password" required placeholder="비밀번호 입력")
-        
+
+        p.errMsg(v-if="successLogin") 이메일 혹은 비밀번호를 다시 한번 확인해주세요
+
         button.btn.btn-primary.w-100.mb-2(type="submit") 로그인
-  
-      button.btn.btn-outline-secondary.w-100(@click="goToSignup") 회원가입
+        button.btn.btn-outline-secondary.w-100(@click="goToSignup") 회원가입
   </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { useRouter } from 'vue-router'
-  import api from '../lib/axios'
-  
-  const router = useRouter()
-  const email = ref('')
-  const password = ref('')
-  
-  const handleLogin = async () => {
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import api from "../lib/axios";
+
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const successLogin = ref(false);
+
+const handleLogin = async () => {
   if (!email.value || !password.value) {
-    alert('이메일과 비밀번호를 입력해주세요.')
-    return
+    alert("이메일과 비밀번호를 입력해주세요.");
+    return;
   }
 
   try {
-    const res = await api.post('/login', {
+    const res = await api.post("/login", {
       email: email.value,
-      password: password.value
-    })
+      password: password.value,
+    });
 
-    console.log('로그인 성공: ', res);
+    console.log("로그인 성공: ", res);
     const token = res.data.token;
-    localStorage.setItem('token', token);
-    router.push('/workspace');
-
+    localStorage.setItem("token", token);
+    router.push("/workspace");
   } catch (err) {
-    console.error('로그인 실패:', err.response?.data || err.message)
-    alert('로그인 실패: ' + (err.response?.data?.error || '오류 발생'))
+    console.error("로그인 실패:", err.response?.data || err.message);
+    successLogin.value = true;
   }
+};
+
+const goToSignup = () => {
+  router.push("/signup");
+};
+</script>
+
+<style scoped>
+.errMsg {
+  color: red;
 }
-  
-  const goToSignup = () => {
-    router.push('/signup')
-  }
-  </script>
-  
-  <style scoped>
-  </style>
-  
+</style>
